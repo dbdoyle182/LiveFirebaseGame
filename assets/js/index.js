ae62db1df53e243c407b79511efdaf65aca16b95
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-17 15:04:05 -0400

    initialized firebase

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBrCWkH7ccwpAIA7Ox8zAzW2q6Xc55PgQk",
+    authDomain: "onlinerps-d8af0.firebaseapp.com",
+    databaseURL: "https://onlinerps-d8af0.firebaseio.com",
+    projectId: "onlinerps-d8af0",
+    storageBucket: "onlinerps-d8af0.appspot.com",
+    messagingSenderId: "98602852390"
+  };
+  firebase.initializeApp(config);
